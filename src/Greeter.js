816bdb020806b4ed5b2cf5e919e796c3b0bfe950
Mohan Kumar <mohan.kumar@inmobi.com>
2014-08-18 14:56:24 +0530

    my commit

--- src/Greeter.js
+++ src/Greeter.js
@@ -6,4 +6,7 @@ myapp.Greeter = function() {
 myapp.Greeter.prototype.greet = function(name) {
     return   name + " !";
 };
+myapp.Greeter.prototype.greet = function (name) {
+    return   name + " !";
+};
 
