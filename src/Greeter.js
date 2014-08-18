800c46b1687b77be7217e509e52e086dad526086
jmaovhasncript <maddyrammohan@gmail.com>
2014-08-18 14:57:37 +0530

    merge conf

--- src/Greeter.js
+++ src/Greeter.js
@@ -6,7 +6,7 @@ myapp.Greeter = function() {
 myapp.Greeter.prototype.greet = function(name) {
     return   name + " !";
 };
-myapp.Greeter.prototype.greet = function (name) {
+myapp.Greeter.prototype.greet1 = function (name) {
     return   name + " !";
 };
 
